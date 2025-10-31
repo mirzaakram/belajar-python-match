412c060fe98eb0722bdcff3d7f872809257b2f8f
mirzaakram <zeinspace30@gmail.com>
2025-10-31 08:33:31 +0700

    Add files via upload

---
+++ belajar-match.py
@@ -0,0 +1,63 @@
+#match
+Hari = 6
+match Hari:
+    case 1:
+        print("senin")
+    case 2:
+        print("selasa")
+    case 3:
+        print("rabu")
+    case 4:
+        print("kamis")
+    case 5:
+        print("jum'at")
+    case 6:
+        print("sabtu")
+    case 7:
+        print("minggu")
+
+
+
+Bulan = 6
+match Hari:
+    case 1:
+        print("januari")
+    case 2:
+        print("februari")
+    case 3:
+        print("maret")
+    case 4:
+        print("april")
+    case 5:
+        print("mei")
+    case 6:
+        print("juni")
+    case 7:
+        print("juli")
+    case 8:
+        print("agustus")
+    case 9:
+        print("september")
+    case 10:
+        print("october")
+    case 11:
+        print("november")
+    case 12:
+        print("desember")
+        
+Bulan = 4
+Hari = 4
+match Hari:
+    case 1 | 2 | 3 | 4 | 5 if Bulan ==4:
+        print("Sekolah di bulan April")
+    case 1 | 2 | 3 | 4 | 5 if Bulan ==5:
+        print("sekolah di bulan mei")
+    case _:
+        print("No match")
+        
+hari = 6
+match hari:
+    case 1 | 2 | 3 | 4 | 5:
+        print("Masukk sekolah")
+    case 6 | 7:
+        print("liburrrrrr")
